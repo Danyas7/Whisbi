a794f8a2205a2b882ba781f0d3dc71149f5516dd
Daniel Salmeron <daniel_salmeron1@homedepot.com>
2021-06-02 23:32:15 -0400

    result now appear on the landing page

---
+++ src/services/currencyApiCall.js
@@ -0,0 +1,44 @@
+function apiCurrency()
+{
+    const baseUrl = 'https://api.exchangeratesapi.io/v1/'
+    const endpoint = 'convert'
+    const date = document.getElementById("date").value
+    const amount = document.getElementById("amount").value
+
+    let firstCurrency = document.getElementById("currency-one").value
+    const firstCurrencySymbol = firstCurrency.slice(0, 3);
+    const firstCurrencyName = firstCurrency.slice(5);
+
+    let secondCurrency = document.getElementById("currency-two").value
+    const secondCurrencySymbol = secondCurrency.slice(0, 3);
+    const secondCurrencyName = secondCurrency.slice(5);
+
+
+
+    let symbols;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${baseUrl}${endpoint}?access_key=391079608fe2657cbc41c006048b93df&from=${firstCurrencySymbol}&to=${secondCurrencySymbol}&amount=${amount}&date=${date}`, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            symbols = JSON.parse(xhr.responseText);
+
+            const resultStatementOne = document.getElementById("result-statement-one");
+            const resultStatementTwo = document.getElementById("result-statement-two");
+            const resultStatementThree = document.getElementById("result-statement-three");
+
+            resultStatementOne.textContent = `1 ${symbols.query.from} equals `;
+            resultStatementOne.value = `1 ${symbols.query.from} equals `;
+
+            resultStatementTwo.textContent = `${symbols.info.rate} ${symbols.query.to}`;
+            resultStatementTwo.value = `${symbols.info.rate} ${symbols.query.to}`;
+
+            resultStatementThree.textContent = `${symbols.query.amount} ${firstCurrencyName} equals ${symbols.result} ${secondCurrencyName}`;
+            resultStatementThree.value = `${symbols.query.amount} ${firstCurrencyName} equals ${symbols.result} ${secondCurrencyName}`;
+        }
+        else if (xhr.status !== 200){
+            console.error(`Request failed. Request status of ${xhr.status}`)
+            alert(`Request failed. Request status of ${xhr.status}`);
+        }
+    }
+    xhr.send("")
+}
