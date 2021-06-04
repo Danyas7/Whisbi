b797e5042807999a5c7cb538fc6797e0f2c484de
Daniel Salmeron <daniel_salmeron1@homedepot.com>
2021-06-03 21:55:36 -0400

    last clean up

--- src/services/currencyApiCall.js
+++ src/services/currencyApiCall.js
@@ -1,5 +1,4 @@
-function apiCurrency()
-{
+function apiCurrency() {
     let symbols;
     const baseUrl = 'https://api.exchangeratesapi.io/v1/'
     const endpoint = 'convert'
@@ -13,8 +12,6 @@ function apiCurrency()
     let secondCurrency = document.getElementById("currency-two").value
     const secondCurrencySymbol = secondCurrency.slice(0, 3);
     const secondCurrencyName = secondCurrency.slice(5);
-
-
 
     const xhr = new XMLHttpRequest();
     xhr.open("GET", `${baseUrl}${endpoint}?access_key=391079608fe2657cbc41c006048b93df&from=${firstCurrencySymbol}&to=${secondCurrencySymbol}&amount=${amount}&date=${date}`, true);
