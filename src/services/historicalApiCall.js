d7cdffc9368b37df572f3f868767a4cab6f06cc4
Daniel Salmeron <daniel_salmeron1@homedepot.com>
2021-06-22 08:53:25 -0400

    just some code clean up

--- src/services/historicalApiCall.js
+++ src/services/historicalApiCall.js
@@ -1,27 +1,12 @@
 async function apiHistorical() {
-    let symbols;
+    // let symbols;
     let datesArray = [];
     let histData = [];
     const baseUrl = 'https://api.exchangeratesapi.io/v1/'
+    const amount = document.getElementById("amount").value;
+
     const startDate = document.getElementById("date").value;
     let endDate = new Date(startDate);
-
-    for (let d = 0; d < 13; d++) {
-        endDate.setMonth(endDate.getMonth("YYYY-MM-DD") - d);
-
-        function formatDate(template, date) {
-            var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':');
-            date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4);
-            return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
-                return template.split(specs[i]).join(item);
-            }, template);
-        }
-
-        let datePoint = formatDate('YYYY-MM-DD', endDate);
-        datesArray.push(datePoint);
-    }
-
-    const amount = document.getElementById("amount").value;
 
     let firstCurrency = document.getElementById("currency-one").value;
     const firstCurrencySymbol = firstCurrency.slice(0, 3);
@@ -31,17 +16,29 @@ async function apiHistorical() {
     const secondCurrencySymbol = secondCurrency.slice(0, 3);
     const secondCurrencyName = secondCurrency.slice(5);
 
-    let symbols;
-    let histData = [];
-    const requests = new Array(datesArray.length);
+    // const requests = new Array(datesArray.length);
 
-    var makeRequest = function (url, method) {
+    const formatDate = (template, date) => {
+        const specs = 'YYYY:MM:DD:HH:mm:ss'.split(':');
+        date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4);
+        return date.toISOString().split(/[-:.TZ]/).reduce((template, item, i) =>
+            template.split(specs[i]).join(item), template);
+    };
+
+    for (let d = 0; d < 13; d++) {
+        endDate.setMonth(endDate.getMonth("YYYY-MM-DD") - d);
+        let datePoint = formatDate('YYYY-MM-DD', endDate);
+        datesArray.push(datePoint);
+    };
+
+
+    const makeRequest = (url, method) => {
 
         // Create the XHR request
-        var request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
         // Return it as a Promise
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
 
             // Setup our listener to process compeleted requests
             request.onreadystatechange = function () {
